30b92db43ca4ba9fc569bb2e58ffff839c28a173
nesirid <nasirid@code.edu.az>
2024-02-22 08:58:43 +0400

    home task

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿////// See https://aka.ms/new-console-template for more information
+////Console.WriteLine("Hello, World!");
+
+//string username, password;
+//    int ctr = 0;
+
+////Console.ReadLine();
+//Console.WriteLine("Read the input username");
+//username = Console.ReadLine(); // Read the input username
+
+//if (username != "abcd")
+//{
+//    Console.WriteLine("Incorrect");   // Increment the login attempt counter
+//}
+
+{
+    static void Users (string[] args);
+
+        Console.WriteLine("Enter username : ");
+    string user = Console.ReadLine();
+
+    Console.WriteLine("Enter password : ");
+    string pass = Console.ReadLine();
+
+    bool isValid = false;
+
+    Console.WriteLine("State : {0}", isValid);
+
+    Console.ReadLine();
+
+}
